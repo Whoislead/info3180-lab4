60189b56d451a7aa35eb756bbb039338829adc46
Whoislead <94552006+Whoislead@users.noreply.github.com>
2024-04-08 10:45:40 -0500

    added update to views.py

--- app/views.py
+++ app/views.py
@@ -1,10 +1,11 @@
 import os
 from app import app, db, login_manager
-from flask import render_template, request, redirect, url_for, flash, session, abort
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
-from app.forms import LoginForm
+from app.forms import LoginForm, UploadForm
+from werkzeug.security import check_password_hash
 
 
 ###
@@ -24,43 +25,56 @@ def about():
 
 
 @app.route('/upload', methods=['POST', 'GET'])
+@login_required
 def upload():
     # Instantiate your form class
-
+    form = UploadForm()
     # Validate file upload on submit
-    if form.validate_on_submit():
-        # Get file data and save to your uploads folder
-
+    if request.method == 'POST' and form.validate_on_submit():
+        # Get file data and save to your folder
+        file = form.file_upload.data
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         flash('File Saved', 'success')
         return redirect(url_for('home')) # Update this to redirect the user to a route that displays all uploaded image files
-
-    return render_template('upload.html')
+    return render_template('upload.html', form=form)
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     form = LoginForm()
-
     # change this to actually validate the entire form submission
     # and not just one field
-    if form.username.data:
+    if request.method == 'POST' and form.validate_on_submit():
         # Get the username and password values from the form.
-
+        username = form.username.data
+        password = form.password.data
         # Using your model, query database for a user based on the username
         # and password submitted. Remember you need to compare the password hash.
         # You will need to import the appropriate function to do so.
         # Then store the result of that query to a `user` variable so it can be
         # passed to the login_user() method below.
-
+        user = UserProfile.query.filter_by(username=username).first()
+        if user is not None and check_password_hash(user.password, password):
         # Gets user id, load into session
-        login_user(user)
-
-        # Remember to flash a message to the user
-        return redirect(url_for("home"))  # The user should be redirected to the upload form instead
+            login_user(user)
+            flash('Logged in successfully', 'success')
+            return redirect(url_for("upload"))
+        else:
+            flash('Invalid username or password. Please try again','danger')
+    flash_errors(form)
     return render_template("login.html", form=form)
 
-# user_loader callback. This callback is used to reload the user object from
-# the user ID stored in the session
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('home'))
+
+
+
+
 @login_manager.user_loader
 def load_user(id):
     return db.session.execute(db.select(UserProfile).filter_by(id=id)).scalar()
@@ -68,6 +82,13 @@ def load_user(id):
 ###
 # The functions below should be applicable to all Flask apps.
 ###
+def get_uploaded_images():
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', './upload')
+    images = []
+    for filename in os.listdir(UPLOAD_FOLDER):
+        if filename.endswith('.jpg') or filename.endswith('.png'):
+            images.append(filename)
+    return images
 
 # Flash errors from the form if validation fails
 def flash_errors(form):
@@ -77,7 +98,17 @@ def flash_errors(form):
                 getattr(form, field).label.text,
                 error
 ), 'danger')
+@app.route('/upload/<filename>')
+def get_image(filename):
+    return send_from_directory(os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER']), filename)
 
+@app.route('/files')
+@login_required
+def files():
+    # Get a list of uploaded image filenames
+    image_files = get_uploaded_images()
+    # Render the files.html template with the list of image filenames
+    return render_template('files.html', image_files=image_files)
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
     """Send your static text file."""
